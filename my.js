e2a25d83b1b4dfaa74db0caa047975f38ad634e1
canhthaomodule1c0221h2 <79823013+canhthaomodule1c0221h2@users.noreply.github.com>
2021-04-09 10:04:23 +0700

    Add files via upload

---
+++ my.js
@@ -0,0 +1,8 @@
+let a = +prompt("nhập số a vào");
+let b = +prompt("nhập vào số b");
+if (a % b == 0) {
+    document.write("a chia hết cho b");
+}
+else {
+    document.write("a không chia hết cho b");
+}
